bd887d361e4eafead53cb3a10c74398d4958e367
Abdelrahman Elsayed <108687433+D3cipherd@users.noreply.github.com>
2023-12-24 18:42:18 +0200

    Update InterruptRequestRegister.v
    ✔️ Fix module logic

--- Verilog/InterruptRequestRegister.v
+++ Verilog/InterruptRequestRegister.v
@@ -1,4 +1,4 @@
-  module InterruptRequestRegister(
+module InterruptRequestRegister(
 
     // inputs from control logic
     input level_or_edge_triggered_mode,
@@ -8,45 +8,38 @@
     input reg [7:0] ir_req_pin,
 
     // output
-    output reg [7:0] interrupt_req_reg
+    output wire [7:0] interrupt_req_reg
 );
-    wire [7:0] ir_req_edge;
+    reg [7:0] ir_req_edge;
     reg [7:0] delayed_ir_req_line;
 
     genvar ir_bit_no;
     generate
     for(ir_bit_no = 0; ir_bit_no < 8; ir_bit_no = ir_bit_no + 1) begin
         // (Positive) Edge triggered
-        always@(ir_req_pin[ir_bit_no], clear_ir_line[ir_bit_no]) begin
+        always@(ir_req_pin[ir_bit_no], posedge clear_ir_line[ir_bit_no]) begin
             if(clear_ir_line[ir_bit_no]) begin
                 delayed_ir_req_line[ir_bit_no] <= 1'b0;
+                ir_req_edge[ir_bit_no] <= 1'b0;
               end
             else if(ir_req_pin[ir_bit_no] == 1'b1) begin
-              delayed_ir_req_line[ir_bit_no] <= 1'b1;
+                delayed_ir_req_line[ir_bit_no] <= 1'b1;
+                ir_req_edge[ir_bit_no] <= (ir_req_pin[ir_bit_no] == 1'b1) & (~delayed_ir_req_line[ir_bit_no] == 1'b1);
               end
             else begin
                 delayed_ir_req_line[ir_bit_no] <= delayed_ir_req_line[ir_bit_no];
               end
                end
-       
-
-     assign ir_req_edge[ir_bit_no] = (ir_req_pin[ir_bit_no] == 1'b1) & (~delayed_ir_req_line[ir_bit_no] == 1'b1);
-    
-                                        
-        // Level Triggered
-        always@(ir_req_pin[ir_bit_no], clear_ir_line[ir_bit_no])begin
-            if(clear_ir_line[ir_bit_no])
-                interrupt_req_reg[ir_bit_no] <= 1'b0;
-            // level -> 1,  edge -> 0
-            else if(level_or_edge_triggered_mode)
-                interrupt_req_reg[ir_bit_no] <= ir_req_pin[ir_bit_no];
-            else 
-                interrupt_req_reg[ir_bit_no] <= ir_req_edge[ir_bit_no];
-        end
-
+      
+       // Level Triggered OR Edge Triggered event selector
+      assign  interrupt_req_reg[ir_bit_no] = (clear_ir_line[ir_bit_no] == 1'b1) ? (1'b0) // Clear Bit
+                                            : (level_or_edge_triggered_mode == 1'b0) ? (ir_req_edge[ir_bit_no])  // Edge Triggered Mode
+                                            : (ir_req_pin[ir_bit_no]); // Level Triggered Mode
+                                       
     end
     endgenerate 
 endmodule   
+
 
 
 
