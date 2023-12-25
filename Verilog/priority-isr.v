8e98900d51af9d54e38ccc1e7c4de5b308e701fa
Abdelrahman Elsayed <108687433+D3cipherd@users.noreply.github.com>
2023-12-25 23:34:33 +0200

    Update priority-isr.v

--- Verilog/priority-isr.v
+++ Verilog/priority-isr.v
@@ -1,112 +1,58 @@
-module ISRAndPriorityResolver (
-  input [7:0] IMR, IRR,
-  input INTA, AEOI, AutomaticRotation, EOI,
-  output reg [7:0] ISR,
-  output wire Prioritywire,
-  output wire INT,
-  output reg clearFlag, PriorityFlagSendTocascading,
-  output reg [2:0] clearBit, PrioritySendtoCascading,
-  output reg defaultIR7
-);
- integer start_index;
-  reg [23:0] priorityLevelRegister;
-  reg [7:0] AcceptedIntrupt;
-  reg [2:0] currenthighestpriorityreg;
-  reg [2:0] currenthighestpriorityregLevel;
-  reg [2:0] counter;
-  reg [2:0] helper;
- integer end_index;
-  assign Prioritywire = 1'b0;
-  assign INT = (AcceptedIntrupt != 0); 
-
-  always @* begin
-    AcceptedIntrupt = IRR & ~IMR;
+module PriorityResolver(
   
-    currenthighestpriorityreg = 3'b111;
-    currenthighestpriorityregLevel = 3'b111;
+  // Inputs from Control Logic
+  input wire [2:0] priority_rotate,
+  input wire [7:0] interrupt_mask,
+  
+  
+  // Other Inputs  
+  input wire [7:0] interrupt_req_reg,
+  input wire [7:0] in_service_register,
+  
+  //Outputs
+  output wire [7:0] interrupt
+);
+  // Functions
+  function  [7:0] rotate(input [7:0] register, input [2:0] rotate_no);
+      case (rotate_no)
+          3'b000:  rotate = { register[0], register[7:1]}; 
+          3'b001:  rotate = { register[1:0], register[7:2]};
+          3'b010:  rotate = { register[2:0], register[7:3]};
+          3'b011:  rotate = { register[3:0], register[7:4]};
+          3'b100:  rotate = { register[4:0], register[7:5]};
+          3'b101:  rotate = { register[5:0], register[7:6]};
+          3'b110:  rotate = { register[6:0], register[7]};
+          3'b111:  rotate = register;
+          default: rotate = register;
+          endcase
+  endfunction
 
-    for (currenthighestpriorityreg = 0; currenthighestpriorityreg < 8; currenthighestpriorityreg = currenthighestpriorityreg + 1) begin
-       start_index = currenthighestpriorityreg * 3 + 2;
-     end_index = currenthighestpriorityreg * 3;
 
-    // Check if the condition is true
-    if (AcceptedIntrupt[currenthighestpriorityreg] && (priorityLevelRegister[start_index:end_index] <= currenthighestpriorityregLevel))  begin
-        currenthighestpriorityregLevel = priorityLevelRegister[currenthighestpriorityreg*3+2:currenthighestpriorityreg*3];
-      end
-    end
+  function  [7:0] un_rotate(input [7:0] register, input [2:0] rotate_no);
+      case (rotate_no)
+          3'b000:  un_rotate = { register[6:0], register[7]}; 
+          3'b001:  un_rotate = { register[5:0], register[7:6]};
+          3'b010:  un_rotate = { register[4:0], register[7:5]};
+          3'b011:  un_rotate = { register[3:0], register[7:4]};
+          3'b100:  un_rotate = { register[2:0], register[7:3]};
+          3'b101:  un_rotate = { register[1:0], register[7:2]};
+          3'b110:  un_rotate = { register[0],   register[7:1]};
+          3'b111:  un_rotate = register;
+          default: un_rotate = register;
+          endcase
+  endfunction
 
-    if (INTA && !INT) begin
-      if (AutomaticRotation) begin
-        helper = 3'b000;
-        for (currenthighestpriorityreg = currenthighestpriorityreg*3+3; currenthighestpriorityreg < 24; currenthighestpriorityreg = currenthighestpriorityreg + 3) begin
-          priorityLevelRegister[currenthighestpriorityreg+2:currenthighestpriorityreg] = helper;
-          helper = helper + 1;
-        end
-        for (currenthighestpriorityreg = 0; currenthighestpriorityreg < currenthighestpriorityreg*3+1; currenthighestpriorityreg = currenthighestpriorityreg + 3) begin
-          priorityLevelRegister[currenthighestpriorityreg+2:currenthighestpriorityreg] = helper;
-          helper = helper + 1;
-        end
-      end else begin
-        helper = 3'b000;
-        for (currenthighestpriorityreg = 0; currenthighestpriorityreg < 24; currenthighestpriorityreg = currenthighestpriorityreg + 3) begin
-          priorityLevelRegister[currenthighestpriorityreg+2:currenthighestpriorityreg] = currenthighestpriorityreg/3;
-        end
-      end
-    end
-  end
+function  [7:0] priority_resolve(input [7:0] register);
+    if      (register[0] == 1'b1)        priority_resolve = 8'b00000001;
+    else if (register[1] == 1'b1)        priority_resolve = 8'b00000010;
+    else if (register[2] == 1'b1)        priority_resolve = 8'b00000100;
+    else if (register[3] == 1'b1)        priority_resolve = 8'b00001000;
+    else if (register[4] == 1'b1)        priority_resolve = 8'b00010000;
+    else if (register[5] == 1'b1)        priority_resolve = 8'b00100000;
+    else if (register[6] == 1'b1)        priority_resolve = 8'b01000000;
+    else if (register[7] == 1'b1)        priority_resolve = 8'b10000000;
+    else                                 priority_resolve = 8'b10000000;
+endfunction
 
-  always @(negedge INTA) begin
-    if (counter == 3'b00) begin
-      clearBit = currenthighestpriorityreg;
-      PrioritySendtoCascading = clearBit;
-      PriorityFlagSendTocascading = clearFlag;
-
-      if (AcceptedIntrupt[currenthighestpriorityreg] == 3'b000) begin
-        clearFlag = 1'b0;
-        ISR = 8'b00000000;
-        clearBit = 3'b000;
-        PrioritySendtoCascading = clearBit;
-        PriorityFlagSendTocascading = clearFlag;
-        defaultIR7 = 1'b1;
-      end else begin
-        clearFlag = 1'b1;
-        ISR[clearBit] = 1'b1;
-      end
-      counter = 3'b01;
-    end else if (counter == 3'b01) begin
-      counter = 3'b10;
-    end
-  end
-
-  always @(posedge INTA) begin
-    if (counter == 3'b10) begin
-      if (AEOI) begin
-        ISR = 8'b00000000;
-      end
-      
-      defaultIR7 = 1'b0;
-      clearFlag = 1'b0;
-      clearBit = 3'b000;
-      PrioritySendtoCascading = clearBit;
-      PriorityFlagSendTocascading = clearFlag;
-      counter = 3'b00;
-    end
-  end
-
-  always @(posedge EOI) begin
-    
-    ISR <= 8'b00000000;
-    counter <= 3'b00;
-    defaultIR7 <= 1'b0;
-    clearFlag <= 1'b0;
-    clearBit <= 3'b000;
-    PrioritySendtoCascading <= clearBit;
-    PriorityFlagSendTocascading <= clearFlag;
-  end 
-
-  initial begin
-    helper = 3'b000;
-    counter = 3'b000;
-    defaultIR7 = 3'b000;
-  end
+  
 endmodule
