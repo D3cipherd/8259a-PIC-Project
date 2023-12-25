9882d85031b98a045177192b8e4579ee4513920d
Zeyad waleed amin <133433704+zeyadwaleed03@users.noreply.github.com>
2023-12-25 23:49:03 +0200

    Update priority-isr.v

--- Verilog/priority-isr.v
+++ Verilog/priority-isr.v
@@ -54,5 +54,43 @@ function  [7:0] priority_resolve(input [7:0] register);
     else                                 priority_resolve = 8'b10000000;
 endfunction
 
+
+
+  
+  ///////// Masked Registers
+  wire [7:0] masked_interrupt_req_reg;
+  wire [7:0] masked_in_service_register;
+    
+  assign masked_interrupt_req_reg = interrupt_req_reg & ~interrupt_mask;
+  assign masked_in_service_register = in_service_register & ~interrupt_mask;
+
+  
+  ///////// Rotation Mode / Fully Nested [priority_rotate = 3'b111 - no rotation]
+  wire [7:0] rotated_interrupt_request;
+  wire [7:0] rotated_interrupt;
+  wire [7:0] rotated_in_service;
+  reg [7:0] in_service_priority_mask;
+
+  
+  assign rotated_interrupt_request = rotate(masked_interrupt_req_reg, priority_rotate);
+  assign rotated_in_service = rotate(in_service_register,priority_rotate);
+
+  always@(rotated_in_service) begin
+    if      (rotated_in_service[0] == 1'b1) in_service_priority_mask = 8'b00000000;
+    else if (rotated_in_service[1] == 1'b1) in_service_priority_mask = 8'b00000001; 
+    else if (rotated_in_service[2] == 1'b1) in_service_priority_mask = 8'b00000011; 
+    else if (rotated_in_service[3] == 1'b1) in_service_priority_mask = 8'b00000111; 
+    else if (rotated_in_service[4] == 1'b1) in_service_priority_mask = 8'b00001111; 
+    else if (rotated_in_service[5] == 1'b1) in_service_priority_mask = 8'b00011111; 
+    else if (rotated_in_service[6] == 1'b1) in_service_priority_mask = 8'b00111111; 
+    else if (rotated_in_service[7] == 1'b1) in_service_priority_mask = 8'b01111111; 
+    else                                    in_service_priority_mask = 8'b11111111;
+  end
+
+
+  assign rotated_interrupt = priority_resolve(rotated_interrupt_request) & in_service_priority_mask;
+
+  assign interrupt = un_rotate(rotated_interrupt, priority_rotate);
+  
   
 endmodule
